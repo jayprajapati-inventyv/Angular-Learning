1926d5e0b52cc29c22cd4849885d02bdcebad5f7
Jay Prajapati <jay.prajapati@inventyv-git.com>
2025-10-13 11:00:51 +0530

    add changes of navbar.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,7 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CartComponent } from './cart-component/cart-component';
 import { AuthService } from './service/auth.service';
-import { Navbar } from './navbar';
+import { Navbar } from './navbar/navbar';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CartComponent, AccountSettings, RouterLink, Navbar],
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { AuthService } from './service/auth.service';
+import { AuthService } from '../service/auth.service';
 
 @Component({
     selector: 'app-navbar',
